25d80f0e03aca0890d79a91f0878e85fde622e46
Rashed-Albatayneh <133786522+Rashed-Albatayneh@users.noreply.github.com>
2023-05-30 09:42:56 -0400

    committed on github 9:42

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -66,16 +66,16 @@ public class CardTrick {
         System.out.println("My name is Rashed, but you can call me prof, Rashed or sir");
         System.out.println();
         
-        System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
+        System.out.println("To be millioner");
+        System.out.println(" i wanr a certificate");
         System.out.println("-- Have a semester with no violations of academic integrity!");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
+        System.out.println("Making money");
+        System.out.println("Football");
         System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Riding my bycle");
 
         System.out.println();
         
